7db99e8df678bb578bca24f0e5636849bcb7efe6
Ben Dean-Kawamura <bdk@mozilla.com>
2026-06-08 18:03:55 -0400

    Upgrade to Uniffi 0.32
    - Bumped UniFFI to 0.32.1
    - Bumped Glean to v70, which also uses UniFFI 0.32
    - Bumped askama and updated Askama filters to work with the new version
    - Bumped toml to the same range as UniFFI (`>=0.9, <2`).
    - Moved custom_type! macro invocations to the root module

--- components/support/nimbus-fml/src/lib.rs
+++ components/support/nimbus-fml/src/lib.rs
@@ -24,3 +24,30 @@ cfg_if::cfg_if! {
 pub mod fixtures;
 
 const SUPPORT_URL_LOADING: bool = true;
+
+#[cfg(feature = "uniffi-bindings")]
+uniffi::custom_type!(JsonObject, String, {
+    remote,
+    try_lift: |val| {
+        let json: serde_json::Value = serde_json::from_str(&val)?;
+
+        match json.as_object() {
+            Some(obj) => Ok(obj.to_owned()),
+            _ => Err(uniffi::deps::anyhow::anyhow!(
+                "Unexpected JSON-non-object in the bagging area"
+            )),
+        }
+    },
+    lower: |obj| serde_json::Value::Object(obj).to_string(),
+});
+
+#[cfg(feature = "uniffi-bindings")]
+uniffi::custom_type!(Url, String, {
+    remote,
+    try_lift: |val| Ok(Self::from_str(&val)?),
+    lower: |obj| obj.as_str().to_string(),
+});
+
+#[cfg(feature = "uniffi-bindings")]
+uniffi::include_scaffolding!("fml");
+
